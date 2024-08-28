03b9075f96a8b647ed9a169150e3ac2515860c39
TheDeadcoder <sakibbuet2k19@gmail.com>
2024-08-28 14:16:20 +0600

    stepped quiz done

--- app/api/api_v1/endpoints/flashcard.py
+++ app/api/api_v1/endpoints/flashcard.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 
 from app.helpers.flashcard_quiz.non_stepped import non_stepped_flashcard_set_generate
+from app.helpers.flashcard_quiz.stepped import stepped_flashcard_set_generate
 from app.schemas.flashcard import flashcardRequest, QuizResponse_non_stepped, QuizResponse_stepped
 from app.helpers.qdrant_functions import file_fetch
 
@@ -19,4 +20,18 @@ async def generate_non_stepped_flashcards(request: flashcardRequest):
     generated_questions = quiz_response.questions[:request.number_of_questions]
 
     return QuizResponse_non_stepped(questions=generated_questions)
+
+@router.post("/step", response_model=QuizResponse_stepped)
+async def generate_non_stepped_flashcards(request: flashcardRequest):
+    
+    if request.number_of_questions <= 0:
+        raise HTTPException(status_code=400, detail="Number of questions must be greater than zero.")
+    
+    knowledge_base = file_fetch(str(request.collection_name), str(request.collection_id), str(request.file_id))
+    print(knowledge_base)
+
+    quiz_response = stepped_flashcard_set_generate(str(request.topic_name),str(request.difficulty_level), request.number_of_questions, str(request.question_type),str(knowledge_base))
+    generated_questions = quiz_response.questions[:request.number_of_questions]
+
+    return QuizResponse_stepped(questions=generated_questions)
 
---
+++ app/helpers/flashcard_quiz/stepped.py
@@ -0,0 +1,37 @@
+
+from app.core.openai import openaiClient
+from app.schemas.flashcard import QuizResponse_stepped
+
+def stepped_flashcard_set_generate(topic_name: str, difficulty_level: str, number_of_questions: int, question_type: str, knowledge_base: str) -> QuizResponse_stepped:
+    system_prompt = f"""
+    You are an AI designed to generate flashcard based quiz. Your goal is to create high-quality questions based on the topic, difficulty level, and type specified. The questions must be relevant, challenging based on the difficulty level, and derived directly from the provided knowledge base.
+    You MUST follow the output format for a question
+    - Question: <The question text>
+    - Answer: <A List of steps each step solving a sub-problem of the main problem>
+    - Step: 
+           - step_name: <Name of the subproblem>
+           - step_solution: <Solution of the sub-problem>
+    - Conclusion: <A brief summary of the steps and the approach to solve the problem>
+    - tips_and_tricks: <Additional tips to tackle similar kind of questions>
+    """
+    human_prompt = f"""
+    Consider these points:
+    - Topic: {topic_name}
+    - Difficulty Level: {difficulty_level} (easy, medium, hard)
+    - Number of Questions: {number_of_questions}
+    - Question Type: {question_type} (e.g., scenario-based, information-based, etc.)
+    - Knowledge Base: The following content is the source material in markdown format
+      {knowledge_base}
+    """
+
+    completion = openaiClient.beta.chat.completions.parse(
+        model="gpt-4o-mini",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": human_prompt},
+        ],
+        response_format=QuizResponse_stepped,
+    )
+    print(completion.choices[0].message.parsed)
+
+    return completion.choices[0].message.parsed
